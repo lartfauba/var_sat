f5572ef25cd1568ef7071ddbebb2538c26ab2ab6
David Wozniak <dvinazza@agro.uba.ar>
2018-06-25 13:03:53 -0300

    Filtro por fecha

--- cargaDB/dbII_main.py
+++ cargaDB/dbII_main.py
@@ -13,6 +13,7 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from datetime import datetime
 # from textwrap import dedent
+from sys import  exit
 import json
 
 from multiprocessing import cpu_count
@@ -72,6 +73,15 @@ dbII_funciones.logger = obtenerLogger(ARGS.logfile)
 #print ARGS.subdatasets
 imagenes = dbII_funciones.buscarImagenes(ARGS.ruta, ARGS.satelite,
                                          ARGS.producto, ARGS.version, ARGS.tile)
+
+# Me quedo sólo con las que estan en fecha
+# MOD13A2.A2012161.h12v12.006.2015247050651.hdf
+
+#if ARGS.start_date or ARGS.end_date:
+#    for i in imagenes:
+#        fecha = datetime.strptime(i.split('/')[-1].split('.')[1] , "A%Y%j")
+#        print(i, fecha)
+#exit()
 
 #print dbII_funciones.listarInventario()
 #print lista_imagenes
